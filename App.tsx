bdf3ba23aba6660811d35946d73207d0f2eb309a
whrksp126 <whrksp126@gmail.com>
2025-08-24 04:34:07 +0900

    save app logo

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ActivityIndicator, View, StatusBar } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
@@ -13,10 +13,18 @@ import { UserProvider } from './src/contexts/UserContext';
 import AuthNavigator from './src/navigation/AuthNavigator';
 import AppNavigator from './src/navigation/AppNavigator';
 
+import RNBootSplash from 'react-native-bootsplash';
+
+
 import "./global.css"; // nativewind
 
 function Main() {
   const { isLoggedIn, loading } = useAuth();
+
+  useEffect(() => {
+    console.log("App.tsx useEffect");
+    RNBootSplash.hide({ fade: true });
+  }, []);
 
   if (loading) {
     return (
